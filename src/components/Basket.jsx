fc48209b343160ef991df139dfed4ae10d05b7d0
Helbob <millehelbo@outlook.dk>
2022-11-12 14:21:21 +0100

    new

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-function Basket() {
+function Basket(props) {
   return (
     <section className="Basket">
       <ul>
-        <li>Product x3, 999,-</li>
+        {props.cart.map((item) => (
+          <li>
+            {item.productdisplayname} x{item.amount} {item.amount * item.price}
+            ,-
+          </li>
+        ))}
       </ul>
       <button>Buy now</button>
     </section>
